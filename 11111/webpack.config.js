6a74d45eb8aee7f8b0aa7e6fd95801c9c328cf4f
Illia Kukharchyk <illich1912@gmail.com>
2019-11-05 22:14:13 +0300

    Commit

---
+++ 11111/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  devServer: {
+    open: true,
+    inline: true,
+    contentBase: './dist',
+    historyApiFallback: true,
+    port: 9000
+  },
+  devtool: 'cheap-module-eval-source-map',
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss/,
+        use: ['style-loader', 'css-loader', { loader: 'sass-loader', options: {} }]
+      },
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: 'html-loader'
+        }
+      }
+    ]
+  },
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "js/bundle.js",
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin()
+  ]
+};
